dbd5ddb5b4f646b288602728fe1d398f0faf18d4
Ryuuichiro07 <159006757+Ryuuichiro07@users.noreply.github.com>
2024-03-07 20:04:37 +0900

    修正１

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nick_name              {Faker::Name.initials(number: 4)}
     email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              { "a1" + Faker::Internet.password(min_length: 4) }
     password_confirmation {password}
     first_name          {"山田"}
     last_name          {"太朗"}

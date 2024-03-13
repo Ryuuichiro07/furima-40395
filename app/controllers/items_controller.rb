e2ea39f757a990239f36f88be911064a136343cd
Ryuuichiro07 <159006757+Ryuuichiro07@users.noreply.github.com>
2024-03-14 04:31:27 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
 
 before_action :authenticate_user!, only: [:new, :create]
+before_action :before_move_to_edit, only: :edit
+  
 
 def index
   @items = Item.order("created_at DESC")
@@ -23,10 +25,31 @@ def show
   @item = Item.find(params[:id])
 end
 
+def edit
+  @item = Item.find(params[:id])
+end
+
+def update
+  @item = Item.find(params[:id])
+  if @item.update(item_params)
+      redirect_to item_path(@item)
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
+
 private
 
 def item_params
   params.require(:item).permit(:name, :explantion, :category_id, :situation_id,:delivery_charge_id, :region_of_origin_id, :number_of_days_until_shipping_id, :price,:image,).merge(user_id: current_user.id)
 end
+
+def before_move_to_edit
+  @item = Item.find(params[:id])
+  unless user_signed_in? && @item.user == current_user
+    redirect_to action: :index
+  end
+end
+
 
 end
